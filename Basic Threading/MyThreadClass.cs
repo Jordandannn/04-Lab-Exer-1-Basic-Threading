448a5e0510f3cdc348ed57a9ea8a75d7a1ab384b
Jordan <Jordan@DESKTOP-7I760FS>
2023-10-15 00:12:29 +0800

    Add project files.

---
+++ Basic Threading/MyThreadClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Basic_Threading
+{
+    internal class MyThreadClass
+    {
+        public static void thread1()
+        {
+            for (int loopCount = 0; loopCount <= 5; loopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Thread.Sleep(1500);
+                Console.WriteLine("Name of the Thread: " +thread.Name+ " = "+ loopCount);
+            }
+        }
+    }
+}
---
+++ Basic Threading/frmBasicThreading.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Threading
+{
+    public partial class frmBasicThreading : Form
+    {
+        public frmBasicThreading()
+        {
+            InitializeComponent();
+            label1.Text = "-Before starting thread-";
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(label1.Text.ToString());
+
+            Thread ThreadA = new Thread(MyThreadClass.thread1);
+            ThreadA.Name = "Thread A Process";
+            ThreadA.Start();
+
+            Thread ThreadB = new Thread(MyThreadClass.thread1);
+            ThreadB.Name = "Thread B Process";
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            label1.Text = "-End of thread-";
+            Console.WriteLine(label1.Text.ToString());
+        }
+    }
+}
